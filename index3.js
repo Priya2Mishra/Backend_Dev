dc752d1c189bc7ffc573d4234895e02a15ef26c1
Priya Mishra <shrijimishra6@gmail.com>
2026-01-19 10:42:21 +0530

    functions_of_js

---
+++ index3.js
@@ -0,0 +1,36 @@
+
+let e = "abc@gmail.com";
+
+console.log(e.split("@"));
+console.log(e.length);
+
+let f = "Shriji";
+console.log(f[5]);
+console.log(f.toUpperCase());
+
+let g = "PRIYA";
+console.log(g.toLowerCase());
+console.log(g.trim());
+console.log(g.includes("v"));
+
+console.log(e.startsWith("abc"));
+console.log(e.endsWith("com"));
+
+console.log(e.slice(3,9));
+console.log(e.replace("com","in"));
+
+let h = "good morning all of you";
+console.log(h.split(" "));
+
+let i = "hii everyone its";
+console.log(i.concat("",f));
+console.log(i.substring(7,14));
+
+
+
+
+
+
+
+
+

50302c9149ec0e72b0bad77fab66121236fb0afa
Priya2Mishra <shrijimishra6@gmail.com>
2026-02-08 22:13:24 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// app.js
-const stringUtils = require('./stringUtils');
-
-const myString = "hello bridgelabz";
-
-console.log("Original:", myString);
-console.log("Capitalized:", stringUtils.capitalize(myString));
-console.log("Reversed:", stringUtils.reverseString(myString));
-console.log("Vowel Count:", stringUtils.countVowels(myString));

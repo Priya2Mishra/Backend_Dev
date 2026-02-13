e9064d8e1c7f252bb1db4d64f4e71aff36f4f348
Priya Mishra <shrijimishra6@gmail.com>
2026-02-13 14:17:47 +0530

    Update express backend

---
+++ Assignment2/index.js
@@ -0,0 +1,14 @@
+let {ReadLog} = require('./Function01');
+let {CapitaliseString, ReverseString, CountoFVowels} = require('./stringutils');
+let {UseSetTimeout, UseSetInterval, useSetImmediate} = require('./Function02');
+
+ReadLog();
+// console.log(CapitaliseString("hello world"));
+// console.log(ReverseString("hello world"));
+// console.log((CountoFVowels("hello world")));
+
+
+// useSetImmediate();
+// UseSetTimeout();cd Backend_Dev/assignment1
+
+// UseSetInterval();git branch -M main

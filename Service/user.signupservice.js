0fd3ad0a83ee6af4752b3f853cdbc2d38473deb1
Priya Mishra <shrijimishra6@gmail.com>
2026-04-10 14:41:13 +0530

    Added authentication authorization backend code

--- Service/user.signupservice.js
+++ Service/user.signupservice.js
@@ -1,23 +1,15 @@
-import createUser from "../controller/user.signupController.js"
-import {StatusCodes} from "http-status-pro-js"
+import bcrypt from "bcrypt";
+import { User } from "../schema/user.schema.js";
 
 
-export default function usersignup(req,res){
+export default async function createUser(name,email,password){
     try {
-        let{name,email,password} = req.body
-        let user = createUser(name,email,password)
-        if(!user){
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR.code).json({
-                code:StatusCodes.INTERNAL_SERVER_ERROR.code,
-                message:StatusCodes.INTERNAL_SERVER_ERROR.message,
-                data:null
-            })
-        }
-        return res.status(StatusCodes.CREATED.code).json({
-            code:StatusCodes.CREATED.code,
-            message:"user added",
-            data:null
-        })
+        let salt = bcrypt.genSalt(10)
+        let hashpassword = bcrypt.hash(password,salt)
+        let newUser = new User({name,email,password:hashpassword})
+        let savedUser = await newUser.save()
+        if(!savedUser) return false
+        return true
     } catch (error) {
         console.log(error)
     }
